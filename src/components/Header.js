919952b269d5def040d9d071f2285bf2fb90582c
IgorFreiredeMorais <igorfreire110@gmail.com>
2021-11-10 18:41:42 -0300

    adicionando header

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+export default ()=> {
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+      <FontAwesome
+        style={{ alignSelf: 'center' }}
+        name="navicon"
+        size={30}
+        color="white"
+      />
+        <FontAwesome
+        style={{ alignSelf: 'center' }}
+        name="futbol-o"
+        size={40}
+        color="white"
+      />
+        <FontAwesome
+        style={{ alignSelf: 'center' }}
+        name="search"
+        size={30}
+        color="white"
+      />
+      </View>
+    </SafeAreaView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#101010',
+    flexDirection:'row',
+    paddingRight:15,
+    paddingLeft:15,
+    paddingBottom:8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomColor:'#FFF',
+    borderBottomWidth:10
+  },
+});
